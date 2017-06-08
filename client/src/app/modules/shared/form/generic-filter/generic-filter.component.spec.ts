af291ab4905f244ef8962cec916c3f1406162f36
Marc Torruella <rollwhistler@gmail.com>
2017-06-08 20:00:44 +0200

    some tests

--- client/src/app/modules/shared/form/generic-filter/generic-filter.component.spec.ts
+++ client/src/app/modules/shared/form/generic-filter/generic-filter.component.spec.ts
@@ -2,10 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedTestModule } from '../../shared-test.module';
 import { GenericFilterComponent } from './generic-filter.component';
 import { InputSearchComponent } from '../input-search/input-search.component';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core'
 
 describe('GenericFilterComponent', () => {
   let component: GenericFilterComponent;
   let fixture: ComponentFixture<GenericFilterComponent>;
+  let titleEl: DebugElement;
+  let expectedTitle: string = 'Hola Caracola';
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,10 +22,16 @@ describe('GenericFilterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(GenericFilterComponent);
     component = fixture.componentInstance;
+    titleEl  = fixture.debugElement.query(By.css('h5'));
+    component.title = expectedTitle;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display expected title', () => {
+    expect(titleEl.nativeElement.textContent).toContain(expectedTitle);
   });
 });
