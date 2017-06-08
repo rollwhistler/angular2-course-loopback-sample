0472bc16ccc230f6933a54ac19af877b40144778
Marc Torruella <rollwhistler@gmail.com>
2017-06-08 15:12:36 +0200

    damn UserApi in tests...

--- client/src/app/modules/user/login/login.component.spec.ts
+++ client/src/app/modules/user/login/login.component.spec.ts
@@ -1,15 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SharedTestModule } from '../../shared/shared-test.module';
 import { LoginComponent } from './login.component';
-import { SDKBrowserModule } from '../../shared/sdk/index';
+import { UserApi } from '../../shared/sdk/services/index';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async(() => {
+    let userApiStub = {
+      login: () =>{}
+    };
     TestBed.configureTestingModule({
-      imports: [SharedTestModule, SDKBrowserModule.forRoot()],
+      imports: [SharedTestModule],
+      providers: [{provide: UserApi, useValue: userApiStub}],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
